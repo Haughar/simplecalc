d7241af915c6bfc515986d9b75f0f44f50c62a90
Ali Haugh <haughar@hotmail.com>
2015-10-07 19:04:38 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Alison Rose Haugh on 10/7/15.
+//  Copyright © 2015 Alison Haugh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
